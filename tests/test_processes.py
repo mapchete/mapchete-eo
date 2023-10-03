47717615e2dc354c7d6fa83b99f562b6db91e45e
Petr Ševčík <petr.sevcik@eox.at>
2023-10-03 18:03:32 +0200

    lint me

--- tests/test_processes.py
+++ tests/test_processes.py
@@ -1,4 +1,5 @@
 import numpy.ma as ma
+import pytest
 
 
 def test_eoxcloudless_8bit_dtype_scale_mapchete(eoxcloudless_8bit_dtype_scale_mapchete):
@@ -10,3 +11,14 @@ def test_eoxcloudless_8bit_dtype_scale_mapchete(eoxcloudless_8bit_dtype_scale_ma
     assert isinstance(output, ma.MaskedArray)
     assert not output.mask.any()
     assert ma.mean(output) < 100
+
+
+def test_eoxcloudless_mosaic_mapchete(eoxcloudless_mosaic_mapchete):
+    mp = eoxcloudless_mosaic_mapchete.mp()
+    zoom = max(mp.config.zoom_levels)
+    # # tile containing data
+    tile = next(mp.get_process_tiles(zoom))
+    output = mp.execute(tile)
+    assert isinstance(output, ma.MaskedArray)
+    assert not output.mask.any()
+    assert ma.mean(output) > 700

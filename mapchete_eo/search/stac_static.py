5a6636f201541e712c6d9f58bc3841a712a78800
Petr Ševčík <petr.sevcik@eox.at>
2024-03-12 09:39:07 +0100

    force endpoint arg to STACStaticCatalog, TODO make baseurl and endpoint interchangable maybe for these

--- mapchete_eo/search/stac_static.py
+++ mapchete_eo/search/stac_static.py
@@ -23,8 +23,8 @@ StacIO.set_default(FSSpecStacIO)
 class STACStaticCatalog(Catalog):
     def __init__(
         self,
+        endpoint: str,
         baseurl: MPathLike,
-        endpoint: Optional[MPathLike] = None,
         time: Optional[Union[TimeRange, List[TimeRange]]] = None,
         bounds: Bounds = None,
         **kwargs,

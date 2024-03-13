9edc91a1620acd2ce36c84580bbb2dc88c801051
Joachim Ungar <joachim.ungar@eox.at>
2024-03-13 11:41:01 +0100

    fix region-wise parameter getter

--- mapchete_eo/processes/eoxcloudless_mosaic_merge.py
+++ mapchete_eo/processes/eoxcloudless_mosaic_merge.py
@@ -69,7 +69,7 @@ def execute(
                 assets=assets,
                 **dict(
                     default_mosaic_settings,
-                    **region_specific_mosaic_settings.get(region, {})
+                    **region_specific_mosaic_settings.get(region_name, {})
                 )
             )
             if mosaic.mask.all():

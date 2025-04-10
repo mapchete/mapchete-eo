375e1d296c6b3d8ba475b7015dddf347b85791f0
Petr Ševčík <petr.sevcik@eox.at>
2025-04-10 15:10:51 +0200

    test_io_masks as test_sentinel2_masks.py

--- tests/test_sentinel2_masks.py
+++ tests/test_sentinel2_masks.py
@@ -1,12 +1,61 @@
+from mapchete.io.raster import ReferencedRaster
+import numpy.ma as ma
 import pytest
 import xarray as xr
 
-from mapchete_eo.types import MergeMethod
+
+from mapchete_eo.platforms.sentinel2.config import MaskConfig
 from mapchete_eo.platforms.sentinel2.masks import (
-    product_masks_to_slices,
     masks_to_xarray,
+    product_masks_to_slices,
+    read_masks,
 )
 from mapchete_eo.platforms.sentinel2.product import S2Product
+from mapchete_eo.types import MergeMethod
+
+
+def test_read_single_product_mask(s2_stac_item, test_tile):
+    product = S2Product(s2_stac_item)
+    product_read_kwargs = dict(
+        mask_config=MaskConfig.parse(
+            dict(
+                footprint=True,
+                l1c_cloud_type="all",
+                cloud_probability_threshold=15,
+                cloud_probability_resolution=60,
+            )
+        )
+    )
+    raster = product.get_mask(
+        grid=test_tile,
+        mask_config=product_read_kwargs["mask_config"],
+    )
+    assert isinstance(raster, ReferencedRaster)
+    assert raster.array.any()
+    assert not raster.array.all()
+
+
+def test_read_masks(s2_stac_item, test_tile):
+    product = S2Product(s2_stac_item)
+    product_read_kwargs = dict(
+        mask_config=MaskConfig.parse(
+            dict(
+                footprint=True,
+                l1c_cloud_type="all",
+                cloud_probability_threshold=15,
+                cloud_probability_resolution=60,
+            )
+        )
+    )
+    arr = read_masks(
+        products=[product],
+        grid=test_tile,
+        nodatavals=[0, 0, 0],
+        product_read_kwargs=product_read_kwargs,
+    )
+    assert isinstance(arr, ma.MaskedArray)
+    assert arr.any()
+    assert not arr.mask.all()
 
 
 @pytest.mark.parametrize(

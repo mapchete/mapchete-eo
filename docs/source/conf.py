8a4c34e27923bce5ed349efb3ab323f3adf64682
Scartography <sevcik.cartography@gmail.com>
2026-01-09 15:24:19 +0100

    try to fix the pages deployments

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -56,6 +56,7 @@ extensions = [
     "sphinx.ext.viewcode",
     "sphinx.ext.autosummary",
     "sphinx.ext.intersphinx",
+    "sphinx_rtd_theme",
 ]
 
 intersphinx_mapping = {
@@ -77,7 +78,7 @@ templates_path = ["_templates"]
 # exclude_patterns = []
 
 html_theme = "sphinx_rtd_theme"
-html_static_path = ["_static"]
+html_static_path = ["html"]
 # docs/source/conf.py
 
 # Tells Sphinx to be strict, but ignore these specific ambiguous targets
